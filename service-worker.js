d55ee84b4f124cc31ccec604ce070ce35859a023
Emin Muhammadi <muemin17631@sabah.edu.az>
2020-02-21 02:06:53 +0400

    APK testing

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@ workbox.googleAnalytics.initialize();
 
 workbox.core.setCacheNameDetails({
   prefix: 'emiga-tech',
-  suffix: 'v1.2.11',
+  suffix: 'v1.2.12',
   precache: 'emiga-tech-custom-precache-name',
   runtime: 'emiga-tech-custom-runtime-name'
 });
