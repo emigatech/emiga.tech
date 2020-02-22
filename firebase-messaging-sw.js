1dd8fef4260e696957033314ccf8d08ad1ad6f67
Emin Muhammadi <muemin17631@sabah.edu.az>
2020-02-22 16:17:02 +0400

    Notifications fixed

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,7 +2,14 @@ importScripts('https://www.gstatic.com/firebasejs/7.8.2/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/7.8.2/firebase-messaging.js');
 
 firebase.initializeApp({
-  'messagingSenderId': '690759506484'
+	apiKey: "AIzaSyBJgSkE-MEvoYobuDV5KTHVLSvTmOjoV10",
+	authDomain: "emiga-tech.firebaseapp.com",
+	databaseURL: "https://emiga-tech.firebaseio.com",
+	projectId: "emiga-tech",
+	storageBucket: "emiga-tech.appspot.com",
+	messagingSenderId: "690759506484",
+	appId: "1:690759506484:web:5870b0c9535291aabe8861",
+	measurementId: "G-7CFKSN3R8W"
 });
 
 const messaging = firebase.messaging();
