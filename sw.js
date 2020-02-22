80725f567343fc75b1cf6389686bc7dae5205cb7
Emin Muhammadi <muemin17631@sabah.edu.az>
2020-02-22 15:37:07 +0400

    Notifications added

--- sw.js
+++ sw.js
@@ -4,6 +4,19 @@
 importScripts('https://www.gstatic.com/firebasejs/7.8.2/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/7.8.2/firebase-messaging.js');
 
+var firebaseConfig = {
+  apiKey: "AIzaSyBJgSkE-MEvoYobuDV5KTHVLSvTmOjoV10",
+  authDomain: "emiga-tech.firebaseapp.com",
+  databaseURL: "https://emiga-tech.firebaseio.com",
+  projectId: "emiga-tech",
+  storageBucket: "emiga-tech.appspot.com",
+  messagingSenderId: "690759506484",
+  appId: "1:690759506484:web:5870b0c9535291aabe8861",
+  measurementId: "G-7CFKSN3R8W"
+};
+
+firebase.initializeApp(firebaseConfig);
+
 const messaging = firebase.messaging();
 
 messaging.getToken().then((currentToken) => {
