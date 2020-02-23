613b0c5c4ec9f54417b93ad21ce1fd74dd71539b
Emin Muhammadi <muemin17631@sabah.edu.az>
2020-02-23 21:44:33 +0400

    ads

--- assets/js/script.js
+++ assets/js/script.js
@@ -150,29 +150,6 @@ $( document ).ready(function() {
 			</br>
 		<h6>Please check your network connection.</h6>`);
     }
-  })
-  .done(function(data){
-
-      	for(var i = 0; i < data.articles.length; i++)
-      	{
-			(function(w, d, n, s, t) {
-			    w[n] = w[n] || [];
-			    w[n].push(function() {
-				    Ya.Context.AdvManager.render({
-				        blockId: "R-A-518913-2",
-				        renderTo: "emiga_hello_"+i,
-				        async: true,
-				        pageNumber: i
-				    });
-				});
-				t = d.getElementsByTagName("script")[0];
-				s = d.createElement("script");
-				s.type = "text/javascript";
-				s.src = "//an.yandex.ru/system/context.js";
-				s.async = true;
-				t.parentNode.insertBefore(s, t);
-			})(this, this.document, "yandexContextAsyncCallbacks");		
-      	}		    
   });
 
 	$(window).scroll(function(e) {
