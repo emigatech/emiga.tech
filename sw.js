d4e5c34396d5d3f928ed0c5afd4c1110c09ff324
Emin Muhammadi <muemin17631@sabah.edu.az>
2020-02-23 23:00:08 +0400

    pwa

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 //This is the service worker with the Advanced caching
 
-const CACHE = ["emiga-tech-v-1.4.3"];
+const CACHE = "emiga-tech-v-1.4.3";
 
 const precacheFiles = [
   "index.html",
@@ -83,7 +83,6 @@ self.addEventListener("install", function (event) {
       console.log("[emiga.tech] Caching pages during install");
 
       return cache.addAll(precacheFiles).then(function () {
-
         return cache.add(offlineFallbackPage);
       });
     })
@@ -93,18 +92,7 @@ self.addEventListener("install", function (event) {
 // Allow sw to control of current page
 self.addEventListener("activate", function (event) {
   console.log("[emiga.tech] Claiming clients for current page");
-  //self.clients.claim()
-  event.waitUntil(
-	caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (CACHE.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
+  event.waitUntil(self.clients.claim());
 });
 
 // If any fetch fails, it will look for the request in the cache and serve it from there first
