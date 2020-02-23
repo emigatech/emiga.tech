5fb9299ce4ce4531cd2500d87aea2003b0acd3fc
Emin Muhammadi <muemin17631@sabah.edu.az>
2020-02-23 20:17:32 +0400

    sw updated

--- sw.js
+++ sw.js
@@ -51,6 +51,7 @@ const networkFirstPaths = [
 const avoidCachingPaths = [
   /* Add an array of regex of paths that shouldn't be cached */
   // Example: /\/api\/.*/
+  "\b(\w*chrome-extension://\w*)\b"
 ];
 
 function pathComparer(requestUrl, pathRegEx) {
