b8f44940303d8976191147757c036fff1f1105c2
Emin Muhammadi <muemin17631@sabah.edu.az>
2020-02-23 21:25:21 +0400

    ads

--- assets/js/script.js
+++ assets/js/script.js
@@ -138,26 +138,6 @@ $( document ).ready(function() {
 	        $('#emiga-app').append(PostHtml(String(i),String(date),String(url),String(title),String(image),String(content),String(description),String(author),String(source_name)));
 	        lazy.update();
       	}
-      	for(var i = 0; i < data.articles.length; i++)
-      	{
-			(function(w, d, n, s, t) {
-			    w[n] = w[n] || [];
-			    w[n].push(function() {
-			       Ya.Context.AdvManager.render({
-			           blockId: "R-A-518913-2",
-			           renderTo: "emiga_hello_"+i,
-			           async: true,
-			           pageNumber: i
-			       });
-			    });
-			    t = d.getElementsByTagName("script")[0];
-			    s = d.createElement("script");
-			    s.type = "text/javascript";
-			    s.src = "//an.yandex.ru/system/context.js";
-			    s.async = true;
-			    t.parentNode.insertBefore(s, t);
-			})(this, this.document, "yandexContextAsyncCallbacks");
-      	}
     },
 
     error: function(req, status, err) {
