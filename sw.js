6338e5144b73bfc7a30fbbd1824a5369cb91c9cb
Emin Muhammadi <muemin17631@sabah.edu.az>
2020-02-23 17:56:29 +0400

    PWA update

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ const precacheFiles = [
   "images/emiga-logo.png",
   "assets/dist/script.min.js",
   "assets/cdn/bootstrap.min.css".
-  "assets/cdn/aos.css".
+  "assets/cdn/aos.css",
   "assets/cdn/jquery-3.4.1.min.js",
   "assets/cdn/popper.min.js",
   "assets/cdn/bootstrap.min.js",
