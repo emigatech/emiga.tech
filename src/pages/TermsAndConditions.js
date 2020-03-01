d81dc847a3204d836cc73f84fe36294741dcf79e
Emin Muhammadi <muemin17631@sabah.edu.az>
2020-03-01 16:29:35 +0400

    index

--- src/pages/TermsAndConditions.js
+++ src/pages/TermsAndConditions.js
@@ -44,21 +44,24 @@ class TermsAndConditions extends Component {
 
               <p>2.3 As a user of this Site, you agree not to:
                   Systematically retrieve data or other content from the Site to a compile database or directory without written permission from us
-                  Use the Site to advertise or sell goods and services   Copy or adapt the Site&rsquo;s software, including but not limited to Flash, PHP, HTML, JavaScript, or other code
-                  Use, launch, or engage in any automated use of the system, such as using scripts to send comments or messages, robots, scrapers, offline readers, or similar data gathering and extraction tools
-                  Advertise products or services not intended by us   Make any unauthorized use of the Site, including collecting usernames and/or email addresses of users to send unsolicited email or creating user accounts under false pretenses
-                  Use a buying agent or purchasing agent to make purchases on the Site   Circumvent, disable, or otherwise interfere with security-related features of the Site, including features that prevent or restrict the use or copying of any content or enforce limitations on the use
-                  Engage in unauthorized framing of or linking to the Site   Trick, defraud, or mislead us and other users, especially in any attempt to learn sensitive account information such as user passwords   Make improper use of our support services, or submit false reports of abuse or misconduct
-                  Engage in any automated use of the system, such as using scripts to send comments or messages, or using any data mining, robots, or similar data gathering and extraction tools   Interfere with, disrupt, or create an undue burden on the Site or the networks and services connected to the Site
-                  Attempt to impersonate another user or person, or use the username of another user   Sell or otherwise transfer your profile
-                  Use any information obtained from the Site in order to harass, abuse, or harm another person   Use the Site or our content as part of any effort to compete with us or to create a revenue-generating endeavor or commercial enterprise
-                  Harass, annoy, intimidate, or threaten any of our employees, agents, or other users   Attempt to access any portions of the Site that you are restricted from accessing
-                  Decipher, decompile, disassemble, or reverse engineer any of the software comprising or in any way making up a part of the Site
-                  Delete the copyright or other proprietary rights notice from any of the content
-                  Upload or transmit (or attempt to upload or to transmit) viruses, Trojan horses, or other material that interferes with any party&rsquo;s uninterrupted use and enjoyment of the Site, or any material that acts as a passive or active information collection or transmission mechanism
-                  Use the Site in a manner inconsistent with any applicable laws or regulations   Threaten users with negative feedback or offering services solely to give positive feedback to users   Misrepresent experience, skills, or information about a User
-                  Disparage, tarnish, or otherwise harm, in our opinion, us and/or the Site
-                  Falsely imply a relationship with us or another company with whom you do not have a relationship</p>
+                  Use the Site to advertise or sell goods and services;
+                  Copy or adapt the Site&rsquo;s software, including but not limited to Flash, PHP, HTML, JavaScript, or other code;
+                  Use, launch, or engage in any automated use of the system, such as using scripts to send comments or messages, robots, scrapers, offline readers, or similar data gathering and extraction tools;
+                  Advertise products or services not intended by us   Make any unauthorized use of the Site, including collecting usernames and/or email addresses of users to send unsolicited email or creating user accounts under false pretenses;
+                  Use a buying agent or purchasing agent to make purchases on the Site   Circumvent, disable, or otherwise interfere with security-related features of the Site, including features that prevent or restrict the use or copying of any content or enforce limitations on the use;
+                  Engage in unauthorized framing of or linking to the Site   Trick, defraud, or mislead us and other users, especially in any attempt to learn sensitive account information such as user passwords   Make improper use of our support services, or submit false reports of abuse or misconduct;
+                  Engage in any automated use of the system, such as using scripts to send comments or messages, or using any data mining, robots, or similar data gathering and extraction tools   Interfere with, disrupt, or create an undue burden on the Site or the networks and services connected to the Site;
+                  Attempt to impersonate another user or person, or use the username of another user   Sell or otherwise transfer your profile;
+                  Use any information obtained from the Site in order to harass, abuse, or harm another person   Use the Site or our content as part of any effort to compete with us or to create a revenue-generating endeavor or commercial enterprise;
+                  Harass, annoy, intimidate, or threaten any of our employees, agents, or other users   Attempt to access any portions of the Site that you are restricted from accessing;
+                  Decipher, decompile, disassemble, or reverse engineer any of the software comprising or in any way making up a part of the Site;
+                  Delete the copyright or other proprietary rights notice from any of the content;
+                  Upload or transmit (or attempt to upload or to transmit) viruses, Trojan horses, or other material that interferes with any party&rsquo;s uninterrupted use and enjoyment of the Site, or any material that acts as a passive or active information collection or transmission mechanism;
+                  Use the Site in a manner inconsistent with any applicable laws or regulations   Threaten users with negative feedback or offering services solely to give positive feedback to users;
+                  Misrepresent experience, skills, or information about a User;
+                  Disparage, tarnish, or otherwise harm, in our opinion, us and/or the Site;
+                  Falsely imply a relationship with us or another company with whom you do not have a relationship;
+                </p>
 
               <p>3. Information you provide to us</p>
 
