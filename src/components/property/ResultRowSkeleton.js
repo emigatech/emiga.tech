56c7ced71c2793b1a1b17cd7a8c1221adc11e90a
Emin Muhammadi <muemin17631@sabah.edu.az>
2020-03-05 18:01:23 +0400

    bugs fixed

--- src/components/property/ResultRowSkeleton.js
+++ src/components/property/ResultRowSkeleton.js
@@ -1,64 +1,58 @@
 import React, { Component } from "react";
 import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import uuid from 'react-uuid'
+
+function Item() {
+  return(
+      <div className="col-12 border mt-1 mb-1 w-100">
+        <div className="container pt-2 pb-2">
+
+          <div className="d-flex justify-content-between pt-2 pb-2">
+            <div></div>
+            <div>
+              <h6 className="text-muted d-sm-none d-md-block">
+                <Skeleton count={1} width={120}/>
+              </h6>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-sm-12 col-md-4 col-lg-4 mt-1 mb-1 p-0 h-280 rounded-0">
+                <div className="rounded-0 bg-skeleton" style={{height:'280px'}}></div>
+            </div>
+            <div className="col-sm-12 col-md-8 col-lg-8 bg-white p-3 mt-1 mb-1">
+                <h4>
+                  <Skeleton/>
+                </h4>
+                <p className="pt-2 pb-2">
+                    <Skeleton count={2}/>
+                    <Skeleton count={1} width={120}/>
+                </p>
+            </div>
+          </div>
+
+          <div className="d-flex justify-content-between pt-2 pb-2">
+            <div>
+              <Skeleton count={1} width={120}/>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    );
+}
 
 class ResultRowSkeleton extends Component {
 
   render() {
+    const items = [];
+    for (var i=0; i<=3; i++) {
+        items.push(<Item key={uuid()}/>);
+    }
       return (
         <SkeletonTheme color="#dee2e6" highlightColor="#f8f9fa">
           <div className="row">
-
-              <div className="col-12 border mt-1 mb-1 w-100">
-                <div className="container pt-2 pb-2">
-                  <div className="row">
-                    <div className="col-sm-12 col-md-4 col-lg-4 mt-1 mb-1 p-0 h-280 rounded-0">
-                        <Skeleton className="rounded-0" height={280}/>
-                    </div>
-                    <div className="col-sm-12 col-md-8 col-lg-8 bg-white p-3 mt-1 mb-1">
-                        <h4><Skeleton/></h4>
-                        <p className="pt-2 pb-2">
-                            <Skeleton count={2}/>
-                            <Skeleton count={1} width={120}/>
-                        </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              <div className="col-12 border mt-1 mb-1 w-100">
-                <div className="container pt-2 pb-2">
-                  <div className="row">
-                    <div className="col-sm-12 col-md-4 col-lg-4 mt-1 mb-1 p-0 h-280">
-                        <Skeleton className="rounded-0" height={280}/>
-                    </div>
-                    <div className="col-sm-12 col-md-8 col-lg-8 bg-white p-3 mt-1 mb-1">
-                        <h4><Skeleton/></h4>
-                        <p className="pt-2 pb-2">
-                            <Skeleton count={2}/>
-                            <Skeleton count={1} width={120}/>
-                        </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              <div className="col-12 border mt-1 mb-1 w-100">
-                <div className="container pt-2 pb-2">
-                  <div className="row">
-                    <div className="col-sm-12 col-md-4 col-lg-4 mt-1 mb-1 p-0 h-280">
-                        <Skeleton className="rounded-0" height={280}/>
-                    </div>
-                    <div className="col-sm-12 col-md-8 col-lg-8 bg-white p-3 mt-1 mb-1">
-                        <h4><Skeleton/></h4>
-                        <p className="pt-2 pb-2">
-                            <Skeleton count={2}/>
-                            <Skeleton count={1} width={120}/>
-                        </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
+            {items}
           </div>
         </SkeletonTheme>
       );
